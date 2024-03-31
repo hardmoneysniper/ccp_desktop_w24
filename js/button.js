7ce8af4b2db46372561d129535cb275128e7959a
Rongyu Shen <ryushen@umich.edu>
2024-03-30 22:50:18 -0400

    button added

---
+++ js/button.js
@@ -0,0 +1,23 @@
+const formElement = document.querySelector('form');
+
+if (formElement) {
+    formElement.addEventListener('submit', (event) => {
+        const formData = new FormData();
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(result => {
+            console.log('Success:', result);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+        
+        event.preventDefault();
+    })
+}
+
+// credit of https://web.dev/learn/forms/javascript/
